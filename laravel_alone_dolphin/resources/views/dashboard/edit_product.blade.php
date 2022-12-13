326c2e52e5de29e3ca1cd458a27719481129aafe
Đông Phương <20520282@gm.uit.edu.vn>
2022-12-13 23:51:15 +0700

    change layout

--- laravel_alone_dolphin/resources/views/dashboard/edit_product.blade.php
+++ laravel_alone_dolphin/resources/views/dashboard/edit_product.blade.php
@@ -1,55 +1,49 @@
+@extends('components.layout')
+@section('head')
 
-@extends('front.header')
+<title>modifier</title>
+<!-- FONT -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+
+<!-- FONT AWESOME -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!-- OWN CAROUSEL -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+    integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+    integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!-- BS4 CSS-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+<link rel="stylesheet" href="/css/header.css">
+<link rel="stylesheet" href="/dashboard/css/pro_modifier.css">
+
+<style>
+    form {
+        margin-top: 46px;
+    }
+
+    .don-vi {
+        justify-content: space-between
+    }
+
+    .container {
+        margin-top: 60px;
+    }
+</style>
+@endsection
 @section('content')
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>modifier</title>
-
-    <!-- FONT -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;700&display=swap" rel="stylesheet">
-
-    <!-- FONT AWESOME -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- OWN CAROUSEL -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
-        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
-        integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- BS4 CSS-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="/css/header.css">
-    <link rel="stylesheet" href="/dashboard/css/pro_modifier.css">
-
-    <style>
-        form{
-            margin-top: 46px;
-        }
-
-        .don-vi{
-            justify-content: space-between
-        }
-        .container{
-            margin-top: 60px;
-        }
-     
-    </style>
-</head>
 
 
-  
 <form action="/admin/edit/{{ $product->id }}" method="POST">
     @method('PATCH')
     <input type="hidden" name="_token" value="{{csrf_token()}}">
@@ -62,42 +56,46 @@
         <div class="row">
             <!-- Phần nội dung bên trái -->
             <div class="col-7">
-              
-                    <div class="row mb-3">
-                        <div class="col-8">
-                            <input name="name" type="text" class="product-name form-control" placeholder="Tên sản phẩm" value="{{ $product->name }}" required>
+
+                <div class="row mb-3">
+                    <div class="col-8">
+                        <input name="name" type="text" class="product-name form-control" placeholder="Tên sản phẩm"
+                            value="{{ $product->name }}" required>
+                    </div>
+                </div>
+
+                <input name="size" class="form-control mb-3" aria-label="With textarea" placeholder="Kích thước"
+                    value="{{ $product->product_details->size }}" style="height: 50px; resize: none;" required>
+
+                <input name="material" class="form-control mb-3" aria-label="With textarea" placeholder="Chất liệu"
+                    value="{{ $product->material }}" style="height: 50px; resize: none;" required>
+
+
+                <h4 class="font-weight-bold mb-3 ">Đơn vị</h4>
+
+                <div class="unit mb-3 ">
+                    <div class="row mb-3 don-vi">
+                        <div class="col-2" style="width: 150px;">
+                            <label style="font-size: 20px; font-weight: bold;"> Giá</label>
+                            <input style="width: 150px;" type="number" name="price" min="0" class="form-control"
+                                placeholder="Giá" value="{{ $product->price }}">
+                        </div>
+
+                        <div class="col-3">
+                            <label style="font-size: 20px; font-weight: bold;"> Số lượng</label>
+                            <input name="qty" type="number" min="1" class="form-control" placeholder="Số lượng"
+                                value="{{ $product->qty }}">
+                        </div>
+
+                        <div class="col-3">
+                            <label style="font-size: 20px; font-weight: bold;">Cân nặng</label>
+                            <input name="weight" type="text" class="form-control" placeholder="Cân nặng"
+                                value="{{ $product->weight }}">
                         </div>
                     </div>
-
-                    <input name="size" class="form-control mb-3" aria-label="With textarea" placeholder="Kích thước" value="{{ $product->product_details->size }}"
-                        style="height: 50px; resize: none;" required>
-
-                    <input name="material" class="form-control mb-3" aria-label="With textarea" placeholder="Chất liệu" value="{{ $product->material }}"
-                        style="height: 50px; resize: none;" required>
+                </div>
 
 
-                    <h4 class="font-weight-bold mb-3 ">Đơn vị</h4>
-
-                    <div class="unit mb-3 ">
-                        <div class="row mb-3 don-vi">
-                            <div class="col-2" style="width: 150px;">
-                                <label style="font-size: 20px; font-weight: bold;"> Giá</label>
-                                <input style="width: 150px;" type="number" name="price" min="0"class="form-control" placeholder="Giá" value="{{ $product->price }}"> 
-                            </div>
-                            
-                            <div class="col-3">
-                                <label style="font-size: 20px; font-weight: bold;"> Số lượng</label>
-                                <input name="qty" type="number" min="1" class="form-control" placeholder="Số lượng"   value="{{ $product->qty }}">
-                            </div>
-                            
-                            <div class="col-3">
-                                <label style="font-size: 20px; font-weight: bold;">Cân nặng</label>
-                                <input name="weight" type="text" class="form-control" placeholder="Cân nặng" value="{{ $product->weight }}">
-                            </div>
-                        </div>
-                    </div> 
-               
-                
             </div>
 
             <!-- phần nội dung bên phải -->
@@ -105,7 +103,8 @@
                 {{-- <div class="array-item" id="load-img">
                     <div class="add-image-btn">
 
-                            <input name="image[]" type="file" class="input-file" id="choose" onchange="changed(this)" multiple
+                        <input name="image[]" type="file" class="input-file" id="choose" onchange="changed(this)"
+                            multiple
                             accept="image/jpeg,image/jpg,image/png,image/gif,application/zip,application/x-zip-compressed,multipart/x-zip,.cbz">
                         <label for="choose">
                             <i class="fa fa-plus" style="font-size: 50px; display: flex; justify-content: center;
@@ -123,15 +122,15 @@
                 </div>
 
             </div>
-       
+
         </div>
     </div>
 </form>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-        </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
 
 
 @endsection
--- laravel_alone_dolphin/resources/views/front/sign-up.blade.php
+++ laravel_alone_dolphin/resources/views/front/sign-up.blade.php
@@ -1,152 +1,113 @@
-
-@extends('front.header')
-@section('content')
-<head>
-    <link rel="stylesheet" href="/front/css/form_validate.css">
+@extends('components.layout')
+@section('head')
+<link rel="stylesheet" href="/front/css/form_validate.css">
 <link rel="stylesheet" href="/front/css/footer.css">
-    <style>
+<style>
+    #background-pattern {
+        height: 5vh;
+        width: 100%;
+        object-fit: cover;
+        opacity: 0.3;
+    }
+
+    input[type=text],
+    input[type=password] {
+        width: 100%;
+        padding: 5px 8px;
+        border-radius: 0;
+        border: 2px solid rgb(113, 113, 113);
+        margin: 5px 0;
+    }
+
+    input[type=text]:focus,
+    input[type=password]:focus {
+        border-radius: 0;
+        outline: none;
+        border: 2px solid black;
+    }
+
+    @media (min-width: 740px) {
         #background-pattern {
-            height: 15vh;
-            width: 100%;
+            height: 100px;
+            width: 1200px;
             object-fit: cover;
             opacity: 0.3;
         }
-
-        input[type=text],
-        input[type=password] {
-            width: 100%;
-            padding: 5px 8px;
-            border-radius: 0;
-            border: 2px solid rgb(113, 113, 113);
-            margin: 5px 0;
-        }
-
-        input[type=text]:focus,
-        input[type=password]:focus {
-            border-radius: 0;
-            outline: none;
-            border: 2px solid black;
-        }
-
-        @media (min-width: 740px) {
-            #background-pattern {
-                height: 150px;
-                width: 1200px;
-                object-fit: cover;
-                opacity: 0.3;
-            }
-        }
-    </style>
-</head>
+    }
+</style>
+@endsection
+@section('content')
 
 
-<body class="w-full md:w-[1200px] m-auto text-gray-700">
+<div class="">
+    <img src="/front/images/background-pattern.jpg" alt="pattern" id="background-pattern">
+</div>
+<div class="px-5 w-full md:w-1/2 m-auto">
+    @if(Session::has('thanhcong'))
+    <div>{{Session::get('thanhcong')}}</div>
+    @endif
+    <h1 class="text-2xl md:text-4xl text-center font-semibold my-5">Đăng ký</h1>
+    <form action="/sign_up" method="POST" id="sign-up">
+        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        <div class="form-group">
+            <label for="name">Họ và tên</label><span><sup class="text-red-500">*</sup></span><br>
+            <input class="form-control" type="text" id="name" name="name">
+            <span class="form-message"></span>
+        </div>
+        <div class="mt-4 form-group">
+            <label for="email">Email</label><span><sup class="text-red-500">*</sup></span><br>
+            <input class="form-control" type="text" id="email" name="email">
+            <span class="form-message"></span>
+            {{-- @error('email')
+            <span class="form-message">{{ $message }}</span>
+            @enderror --}}
+        </div>
+        <div class="mt-4 form-group">
+            <label for="password">Mật khẩu</label><span><sup class="text-red-500">*</sup></span><br>
+            <input class="form-control" type="password" id="password" name="password">
+            <span class="form-message"></span>
+        </div>
+        <div class="mt-4 form-group">
+            <label for="confirm-password">Xác nhận mật khẩu</label><span><sup class="text-red-500">*</sup></span><br>
+            <input class="form-control" type="password" id="confirm-password" name="confirm-password">
+            <span class="form-message"></span>
+        </div>
 
-    <div class="">
-        <img src="/front/images/background-pattern.jpg" alt="pattern" id="background-pattern">
-    </div>
-    <div class="px-5 w-full md:w-1/2 m-auto">
-            @if(Session::has('thanhcong'))
-            <div>{{Session::get('thanhcong')}}</div>
-            @endif
-        <h1 class="text-2xl md:text-4xl text-center font-semibold my-5">Đăng ký</h1>
-        <form action="/sign_up" method="POST" id="sign-up">
-            <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <div class="form-group">
-                <label for="name">Họ và tên</label><span><sup class="text-red-500">*</sup></span><br>
-                <input class="form-control" type="text" id="name" name="name">
-                <span class="form-message"></span>
-            </div>
-            <div class="mt-4 form-group">
-                <label for="email">Email</label><span><sup class="text-red-500">*</sup></span><br>
-                <input class="form-control" type="text" id="email" name="email"> 
-                <span class="form-message"></span>
-                {{-- @error('email')
-                <span class="form-message">{{ $message }}</span>
-                @enderror --}}
-            </div>
-            <div class="mt-4 form-group">
-                <label for="password">Mật khẩu</label><span><sup class="text-red-500">*</sup></span><br>
-                <input class="form-control" type="password" id="password" name="password">
-                <span class="form-message"></span>
-            </div>
-            <div class="mt-4 form-group">
-                <label for="confirm-password">Xác nhận mật khẩu</label><span><sup
-                        class="text-red-500">*</sup></span><br>
-                <input class="form-control" type="password" id="confirm-password" name="confirm-password">
-                <span class="form-message"></span>
-            </div>
-        
         <div class="mt-8">
             <a href="/sign_in">Đã có tài khoản? Đăng nhập tại đây</a>
         </div>
         <div class="flex justify-center mt-8">
             <!-- <input type="submit" value="Tạo tài khoản"
-                class="px-5 py-2 bg-[#ffde59] text-black text-lg md:text-xl cursor-pointer m-auto"> -->
+                    class="px-5 py-2 bg-[#ffde59] text-black text-lg md:text-xl cursor-pointer m-auto"> -->
 
-                {{-- <button type="submit" class="px-5 py-2 bg-[#ffde59] text-black text-lg md:text-xl cursor-pointer m-auto"
-                Tạo tài khoản
-                > --}}
+            {{-- <button type="submit"
+                class="px-5 py-2 bg-[#ffde59] text-black text-lg md:text-xl cursor-pointer m-auto" Tạo tài khoản> --}}
 
-                {{-- <button type="submit" class="px-5 py-2 bg-[#ffde59] text-black text-lg md:text-xl cursor-pointer m-auto">
+                {{-- <button type="submit"
+                    class="px-5 py-2 bg-[#ffde59] text-black text-lg md:text-xl cursor-pointer m-auto">
                     Tạo tài khoản
                 </button> --}}
                 <input type="submit" class="px-5 py-2 bg-[#ffde59] text-black text-lg md:text-xl cursor-pointer m-auto"
-                   value="Tạo tài khoản">
-             
-            
+                    value="Tạo tài khoản">
+
+
         </div>
     </form>
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<!-- footer -->
-<div id="footer" class=" footer h-fit pb-46">
-    <div class="w-[1200px] m-auto grid grid-cols-3">
-        <div class="footer-item">
-            <div class="head-footer font-medium my-2">Về chúng tôi</div>
-            <div class="text-gray-700 text-sm">
-                <div class="footer-text"><a href="#">Project này</a></div>
-                <div class="footer-text"><a href="#">Thành viên</a></div>
-            </div>
-
-        </div>
-        <div class="footer-item">
-            <div class="head-footer font-medium my-2">Sản phẩm</div>
-            <div class="text-gray-700 text-sm">
-                <div class="footer-text"><a href="#">Giường</a></div>
-                <div class="footer-text"><a href="#">Bàn</a></div>
-                <div class="footer-text"><a href="#">Ghế</a></div>
-                <div class="footer-text"><a href="#">Chăn ga gối</a></div>
-            </div>
-        </div>
-        <div class="footer-item">
-            <div class="head-footer font-medium my-2">Liên hệ</div>
-            <div class="text-gray-700 text-sm">
-                <div class="footer-text">Sđt: 090292xxxx</div>
-                <div class="footer-text">Email: 2052xxxx@gm.uit.edu.vn</div>
-                <div class="footer-text">Địa chỉ: trường đại học Công nghệ Thông tin</div>
-                <div class="link flex gap-5 mt-2">
-                    <a href="#"><img src="/front/images/icon/facebook.svg" alt="" height="30" width="30"></a>
-                    <a href="#"><img src="/front/images/icon/youtube.svg" alt="" height="30" width="30"></a>
-                    <a href="#"><img src="/front/images/icon/behance.svg" alt="" height="30" width="30"></a>
-                    <a href="#"><img src="/front/images/icon/wordpress.svg" alt="" height="30" width="30"></a>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
-    <!-- form validate -->
-    <script src="/front/javascript/form_validate.js"></script>
-    <script>
-        Validator({
-            form: '#sign-up',
 
-            formGroup: '.form-group',
 
-            errorSelector:'.form-message',
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- form validate -->
+<script src="/front/javascript/form_validate.js"></script>
+<script>
+    Validator({
+        form: '#sign-up',
 
-            rules:[
+        formGroup: '.form-group',
+
+        errorSelector: '.form-message',
+
+        rules: [
             Validator.isRequired('#name'),
 
             Validator.isRequired('#email'),
@@ -155,18 +116,18 @@
 
             Validator.isRequired('#password'),
 
-            Validator.minLength('#password',6),
+            Validator.minLength('#password', 6),
 
             Validator.isRequired('#confirm-password'),
 
-            Validator.isConfirmed('#confirm-password',function(){
-                  return document.querySelector('#sign-up #password').value
-                },'Mật khẩu nhập lại không chính xác!'),
-            ],
+            Validator.isConfirmed('#confirm-password', function () {
+                return document.querySelector('#sign-up #password').value
+            }, 'Mật khẩu nhập lại không chính xác!'),
+        ],
 
-            onSubmit:function(data){
-              console.log(data);    
-            }
-        })
-    </script>
+        onSubmit: function (data) {
+            console.log(data);
+        }
+    })
+</script>
 @endsection
