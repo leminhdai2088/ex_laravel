24ab35aefff98006afe66e4633d47388a381dab9
Lê Minh Đại <20521153@gm.uit.edu.vn>
2023-03-11 00:06:36 +0700

    update

--- laravel_alone_dolphin/resources/views/components/footer.blade.php
+++ laravel_alone_dolphin/resources/views/components/footer.blade.php
@@ -40,10 +40,10 @@
                     <div class="footer-text">Email: 2052xxxx@gm.uit.edu.vn</div>
                     <div class="footer-text">Địa chỉ: trường đại học Công nghệ Thông tin</div>
                     <div class="link flex gap-5 mt-2">
-                        <a href="#"><img src="/front/images/icon/facebook.svg" alt="" height="30" width="30"></a>
-                        <a href="#"><img src="/front/images/icon/youtube.svg" alt="" height="30" width="30"></a>
-                        <a href="#"><img src="/front/images/icon/behance.svg" alt="" height="30" width="30"></a>
-                        <a href="#"><img src="/front/images/icon/wordpress.svg" alt="" height="30" width="30"></a>
+                        <a href="https://www.facebook.com/profile.php?id=100047664311320" target="_blank"><img src="/front/images/icon/facebook.svg" alt="Facebook" height="30" width="30" title="Facebook"></a>
+                        <a href="#" target="_blank"><img src="/front/images/icon/youtube.svg" alt="Youtube" height="30" width="30" title="Youtube"></a>
+                        <a href="https://www.instagram.com/_lmduite/" target="_blank"><img src="/front/images/icon/ins.svg" alt="Instagram" height="30" width="30" title="Instagram"></a>
+                        <a href="https://nguyentuankha.wordpress.com/" target="_blank"><img src="/front/images/icon/wordpress.svg" alt="Wordpress" height="30" width="30" title="Wordpress"></a>
                     </div>
                 </div>
             </div>
