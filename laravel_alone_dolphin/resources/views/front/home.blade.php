52419ad470c2326c7bd023a6544d280b16f78092
Quoc Ky <quockynguyen02@gmail.com>
2023-05-15 15:49:20 +0700

    Delete cdn

--- laravel_alone_dolphin/resources/views/front/home.blade.php
+++ laravel_alone_dolphin/resources/views/front/home.blade.php
@@ -1118,7 +1118,6 @@
 
 <script  src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script  src="/front/css/page_home/nivo-slider/jquery.nivo.slider.js"></script>
 <script  src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script  src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
