28b83c5bd187cdca84c6b366cd5c55bb2b238f71
Nguyễn Tuấn Kha <20521427@gm.uit.edu.vn>
2023-05-16 21:55:01 +0700

    UPDATE

--- laravel_alone_dolphin/resources/views/front/blog_detail.blade.php
+++ laravel_alone_dolphin/resources/views/front/blog_detail.blade.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="{{$blog_detail[0]->short_description}}">
+    <meta name="keywords" content="{{$blog_detail[0]->keywords}}">
     <link rel="stylesheet" href="/front/css/footer.css">
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.0/dist/tailwind.min.css">
     <link rel="stylesheet" href="/front/css/page_blog.css">
